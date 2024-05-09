94c5219de70d2b857e8cb831fc29c2ec3ff7293d
BOUCHARD Corentin <corentin.bouchard@etudiant.univ-rennes.fr>
2024-05-09 17:49:20 +0200

    subbytes

--- include/cypher.h
+++ include/cypher.h
@@ -1,4 +1,27 @@
 #ifndef AES_C_CYPHER_H
 #define AES_C_CYPHER_H
 
+#define first_four_bits(char) ((char >>4) & 0x0F)
+#define last_four_bits(char) (char & 0x0F)
+
+struct State{
+    char matrix[4][4];
+};
+//For aes-128 the state is a 4*4 matrix with each cell containing a byte, a state contains a whole block
+
+union Word_128{
+    char words[16];
+    int int_words[4];
+};
+
+extern const char sbox[];
+
+struct State* getState(union Word_128* block);
+void print_word128(union Word_128 word);
+void sub_bytes(struct State* st);
+void shift_rows(struct State* st);
+
+
 #endif //AES_C_CYPHER_H
+
+/**/
--- src/main.c
+++ src/main.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
-#include "../include/keys.h"
-
+#include <stdlib.h>
+#include "../include/cypher.h"
 
 int main(int argc, char* argv[]){
     printf("Hello world!\n");
 
-    union Key_128 mykey;
+    union Word_128 mykey;
     for(int i=0; i!=16; i++){
         mykey.words[i]=i;
     }
-    //print_key(mykey);
+    printf("%d",sizeof(&sbox));
+    struct State* st = getState(&mykey);
 
-    for(int i =0; i!=256;i++){
-        printf("%d\n", sbox[i]);
+    for(int i=0; i!=4;i++) {
+        for (int j = 0; j != 4; j++){
+            printf("%d\n", st->matrix[i][j]);
+        }
     }
+
+    free(st);
 }
 
-void print_key(union Key_128 key){
-    for(int i=0; i!=16; i++){
-        printf("%d,",key.words[i]);
-    }
-    printf("\n");
-    for(int i=0; i!=4; i++){
-        printf("%d,", key.int_words[i]);
-    }
-    printf("\n%d", sizeof(union Key_128));
-}
