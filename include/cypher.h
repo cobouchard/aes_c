50f3188d362a115bd06da97acaf99aeb0cc7223b
BOUCHARD Corentin <corentin.bouchard@etudiant.univ-rennes.fr>
2024-05-20 17:14:39 +0200

    cipher and invcipher implemented, testing required

--- include/cypher.h
+++ include/cypher.h
@@ -27,21 +27,29 @@ struct Key_schedule{
 };
 
 extern const char sbox[];
+extern const char inv_sbox[];
 extern const struct Word_32 Rcon[];
 
-struct State* getState(struct Word_128* block);
+struct State* getState(struct Word_128 block);
+struct Word_128 revert_block(struct State* st);
+
 void print_word128(struct Word_128 word);
 void print_word32(struct Word_32 word);
 struct Word_32 xor_words32(struct Word_32 w1, struct Word_32 w2);
 void rot_word(struct Word_32* word);
 void sub_word(struct Word_32* word);
+char get_sbox_value(char c);
+char get_inv_sbox_value(char c);
 void sub_bytes(struct State* st);
 void shift_rows(struct State* st);
 void mix_columns(struct State* st);
 void key_expansion(struct Round_Key key, struct Key_schedule* result);
 void add_round_key(struct State* st, struct Round_Key key);
-
-
+void inv_shift_rows(struct State* st);
+void inv_sub_bytes(struct State* st);
+void inv_mix_columns(struct State* st);
+void cipher(struct State* st, struct Key_schedule* schedule);
+void inv_cipher(struct State* st, struct Key_schedule* schedule);
 
 #endif //AES_C_CYPHER_H
 
